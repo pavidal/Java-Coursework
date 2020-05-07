0bea5444fc27adb26a92db8ccd57c4db890b82e4
P Ander Vidal <26520527+RegularRolfe@users.noreply.github.com>
2020-05-07 21:43:06 +0100

    Added window listener

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.FileNotFoundException;
+
 public class Main {
-    public static void main(String[] args) {
-        Login l = new Login();
+    public static void main(String[] args) throws FileNotFoundException {
+//        System.out.println(Arrays.deepToString(Database.read(Database.PATH_LOG)));
+        NeoLogin l = new NeoLogin();
+        l.setVisible(true);
+        
+        // When login is successful, the login window is disposed
+        // and detected with this instead of exiting.
+        l.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosed(WindowEvent e) {
+            	System.out.println("Login Successful.");
+            	
+            	// Remove listener as this is a one-time action
+            	l.removeWindowListener(this);
+            	
+            	// TODO: Show main window
+            }
+        });
+    }
+    
+    private static void init() {
+    	System.out.println("afgag");
     }
 }
